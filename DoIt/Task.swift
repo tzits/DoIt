92c7977ddce48409e123e9f52f112d379af2058f
tzits <tzitsman@gmail.com>
2017-05-21 17:55:54 -0400

    some seed data

---
+++ DoIt/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Toby Zitsman on 5/21/17.
+//  Copyright © 2017 Toby Zitsman. All rights reserved.
+//
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var DoItTable: UITableView!
+    
+    var tasks : [Task] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        tasks = makeTasks()
+        
+        DoItTable.dataSource = self
+        DoItTable.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️/(task.name)"
+        } else {
+        cell.textLabel?.text = task.name
+        }
+        return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Go Running"
+        
+        let task2 = Task()
+        task2.name = "Make Pizza"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Yoga"
+        task3.important = true
+        
+        return [task1,task2,task3]
+    }
 }
 
